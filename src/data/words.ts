31435f2f160777bc6e7c3784a547b11c54f6d83e
HabibaAbdelaziz <74095966+HabibaAbdelaziz@users.noreply.github.com>
2025-02-18 23:58:48 +0900

    Create words.ts sample data for testing

---
+++ src/data/words.ts
@@ -0,0 +1,18 @@
+export const wordsData: Record<string, { word: string; image?: string }[]> = {
+    countries: [
+      { word: 'Japan', image: 'https://flagcdn.com/w320/jp.png' },
+      { word: 'Brazil', image: 'https://flagcdn.com/w320/br.png' },
+      { word: 'Canada', image: 'https://flagcdn.com/w320/ca.png' },
+    ],
+    foods: [
+      { word: 'Pizza' },
+      { word: 'Burger' },
+      { word: 'Sushi' },
+    ],
+    movies: [
+      { word: 'Inception' },
+      { word: 'The Matrix' },
+      { word: 'Titanic' },
+    ],
+  };
+  
