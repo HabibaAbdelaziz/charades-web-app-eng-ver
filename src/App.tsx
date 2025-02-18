fd7c290837aacd1109ee3b32a63243e7b79b2a69
HabibaAbdelaziz <74095966+HabibaAbdelaziz@users.noreply.github.com>
2025-02-19 00:18:21 +0900

    Edit App.tsx to include basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/HomePage';
+import Game from './pages/GamePage';
 
-function App() {
-  const [count, setCount] = useState(0)
+//App.tsx is the Root Component where the main layout and routing logic are defined.
+// It defines the structure and navigation of the app
+// Contains the page routing using React Router.
+// Serves as a container for all other components and pages
+// Where it is used? As the top-level componenet that wraps all the other components.
+// App.tsx should include the routing logic using React Router (<Routes> and <Route> components),
+// NOT main.tsx. main.tsx should wrap App with <BrowserRouter> to enable routing, but should not contain any routing logic itself.
 
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/game/:category" element={<Game />} />
+    </Routes>
   )
 }
-
 export default App
